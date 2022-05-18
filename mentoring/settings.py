52b6ba24d5387366faf61a7ccf4f998600190fea
ishamis <ivan.shamis@dataart.com>
2022-05-18 22:27:07 +0300

    Security fix

--- mentoring/settings.py
+++ mentoring/settings.py
@@ -25,8 +25,7 @@ env.read_env(BASE_DIR / ".env")
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-^)ktkvu$t3pkq&s(+*)k!g_f1if8khid^o-95n+_)_fs!ezlpg"
+SECRET_KEY = env.str("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
