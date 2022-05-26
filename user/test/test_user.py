025ae82d3c99464e26973810a89ac0b602489165
ishamis <ivan.shamis@dataart.com>
2022-05-26 17:41:55 +0300

    Added user tests

--- user/test/test_user.py
+++ user/test/test_user.py
@@ -1,9 +1,32 @@
 from rest_framework import status
 
 from .utils import BaseAPITestCase
-from ..models import User
 
 USER_URL = "/api/user/"
+
+
+class UserGetTestCase(BaseAPITestCase):
+    url = USER_URL + "{pk}/"
+
+    def test_get(self):
+        response = self.user.get(self.url.format(pk=self.admin.user_id))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        for field in ["id", "email", "username", "first_name", "last_name"]:
+            self.assertEqual(
+                response.data.get(field), getattr(self.admin.get_user(), field)
+            )
+
+    def test_get_non_auth(self):
+        response = self.user.get(self.url.format(pk=self.admin.user_id), auth=False)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_get_not_exists(self):
+        response = self.user.get(self.url.format(pk=1000))
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_get_bad_pk(self):
+        response = self.user.get(self.url.format(pk="some_string"))
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
 
 class UserGetCurrentTestCase(BaseAPITestCase):
@@ -26,3 +49,38 @@ class UserUpdateCurrentTestCase(BaseAPITestCase):
         response = self.user.put(self.url, data={"username": new_username})
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(self.user.get_user().username, new_username)
+
+    def test_update_me_non_auth(self):
+        response = self.user.put(
+            self.url, data={"username": "very_new_username"}, auth=False
+        )
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_update_me_no_username(self):
+        response = self.user.put(self.url, data={"first_name": "new_first_name"})
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertTrue("errors" in response.data)
+
+    def test_update_me_readonly_email(self):
+        old_email = self.user.get_user().email
+        response = self.user.put(
+            self.url, data={"username": "new_username", "email": "new@emai.com"}
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(self.user.get_user().email, old_email)
+
+    def test_update_me_readonly_is_active(self):
+        old_is_active = self.user.get_user().is_active
+        response = self.user.put(
+            self.url, data={"username": "new_username", "is_active": not old_is_active}
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(self.user.get_user().is_active, old_is_active)
+
+    def test_update_me_readonly_is_staff(self):
+        old_is_staff = self.user.get_user().is_staff
+        response = self.user.put(
+            self.url, data={"username": "new_username", "is_staff": not old_is_staff}
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(self.user.get_user().is_staff, old_is_staff)
